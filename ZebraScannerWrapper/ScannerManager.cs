5b3c3201635f58fd435bd3954c7cd5a4c7dfdf72
monsterkane05 <kane_partridge05@hotmail.com>
2024-10-25 07:55:19 +0100

    Add project files.

---
+++ ZebraScannerWrapper/ScannerManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace ZebraScannerWrapper
+{
+    public class ScannerManager
+    {
+        //Callback Definitions
+        private List<Action<ScanData>> _barcodeScanCallbacks;
+        private List<Action<WeightData>> _weightDataCallbacks;
+        private List<Action<PNPData>> _pnpDataCallbacks;
+
+
+
+        
+
+    }
+}
